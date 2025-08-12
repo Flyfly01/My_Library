3cb9d5ee7189c84bb0fb71df66f392c7f8e7cfa9
Flyfly01 <shannonimbert22@gmail.com>
2025-08-12 19:18:20 -0400

    create function to display book cards

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const bookshelf = document.querySelector(".bookshelf");
+
 const myLibrary = [];
 
 function Book (title, author, pages, read) {
@@ -13,7 +15,31 @@ function addBookToLibrary (title, author, pages, read) {
   myLibrary.push(newBook);
 }
 
+function displayBook (book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const title = document.createElement("h3");
+  title.textContent = book.title;
+
+  const author = document.createElement("p");
+  author.textContent = `by ${book.author}`;
+
+  const pages = document.createElement("p");
+  pages.textContent = `${book.pages} pages`;
+
+  const read = document.createElement("p");
+  read.textContent = book.read ? "Read" : "Not read yet";
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(read);
+
+  bookshelf.appendChild(card);
+}
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
 addBookToLibrary("1984", "George Orwell", 328, true);
 
-console.log(myLibrary);
+myLibrary.forEach(displayBook);
