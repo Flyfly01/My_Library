00f59248ca47c93341336fcb2713ed7fc26f8986
Flyfly01 <shannonimbert22@gmail.com>
2025-08-18 19:47:33 -0400

    create a remove book button

--- index.js
+++ index.js
@@ -7,25 +7,33 @@ const form = document.querySelector("#bookform");
 
 const myLibrary = [];
 
-function Book (title, author, pages, read) {
+function Book (title, author, pages, readstatus) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.readstatus = readstatus;
 }
 
-function addBookToLibrary (title, author, pages, read) {
-  const newBook = new Book (title, author, pages, read);
+/*Book.prototype.changeReadStatus = function () {
+  if (this.readstatus === "Read") {
+    this.readstatus = "Not read yet";
+  } else {
+    this.readstatus = "Read";
+  }
+};*/
+
+function addBookToLibrary (title, author, pages, readstatus) {
+  const newBook = new Book (title, author, pages, readstatus);
   myLibrary.push(newBook);
 }
 
-function displayBook (book) {
+function displayBook (book) { //The book parameter refers to an existing book object in our array
   const card = document.createElement("div");
   card.classList.add("card");
 
   const title = document.createElement("h3");
-  title.textContent = book.title;
+  title.textContent = book.title; //book.title is retrieved from the book object
 
   const author = document.createElement("p");
   author.textContent = `by ${book.author}`;
@@ -33,13 +41,27 @@ function displayBook (book) {
   const pages = document.createElement("p");
   pages.textContent = `${book.pages} pages`;
 
-  const read = document.createElement("p");
-  read.textContent = book.read ? "Read" : "Not read yet";
+  const readstatus = document.createElement("p");
+  readstatus.textContent = book.readstatus ? "Read" : "Not read yet";
+
+  const removeBkButton = document.createElement("button");
+  removeBkButton.classList.add("removeBk");
+  removeBkButton.textContent = "Remove Book";
+
+  /*const readButton = document.createElement("button");
+  readButton.classList.add("readButton");
+  readButton.textContent = "Change read status";
+  readButton.addEventListener("click", () => {
+    book.changeReadStatus;
+    read.textContent = book.readstatus;
+  })*/
 
   card.appendChild(title);
   card.appendChild(author);
   card.appendChild(pages);
-  card.appendChild(read);
+  card.appendChild(readstatus);
+  card.appendChild(removeBkButton);
+  //card.appendChild(readButton);
 
   bookshelf.appendChild(card);
 }
@@ -61,7 +83,7 @@ cancelbtn.addEventListener("click", () => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const formdata = new FormData(form);
+  const formdata = new FormData(form); //retrieves data via name attributes on form elements
   const title = formdata.get("title");
   const author = formdata.get("author");
   const pages = formdata.get("pages");
@@ -69,7 +91,7 @@ form.addEventListener("submit", (e) => {
 
   addBookToLibrary(title, author, pages, readstatus);
 
-  displayBook(myLibrary[myLibrary.length-1]);
+  displayBook(myLibrary[myLibrary.length-1]); //only displaying last book in the array
 
   form.reset();
 
