d4e5253d852b84b0b8267549646996e781143a06
Flyfly01 <shannonimbert22@gmail.com>
2025-08-21 20:03:11 -0400

    style webpage

--- index.js
+++ index.js
@@ -38,8 +38,13 @@ function displayBook (book) { //The book parameter refers to an existing book ob
   const pages = document.createElement("p");
   pages.textContent = `${book.pages} pages`;
 
-  const readstatus = document.createElement("p");
+  const readstatus = document.createElement("div");
   readstatus.textContent = book.readstatus ? "Read" : "Not read yet";
+  if (book.readstatus) {
+    readstatus.classList.add("read");
+  } else {
+    readstatus.classList.add("notread");
+  };
 
   const removeBkButton = document.createElement("button");
   removeBkButton.classList.add("removeBk");
@@ -58,23 +63,40 @@ function displayBook (book) { //The book parameter refers to an existing book ob
   readButton.textContent = "Change read status";
   readButton.addEventListener("click", () => {
     book.changeReadStatus();
-    readstatus.textContent = book.readstatus ? "Read" : "Not read yet";
+    //this "if" statement looks at the NEW INVERTED value of boolean
+    if (book.readstatus) {
+      readstatus.textContent = "Read"; 
+      readstatus.classList.remove("notread");
+      readstatus.classList.add("read");
+    } else {
+      readstatus.textContent = "Not read yet";
+      readstatus.classList.remove("read");
+      readstatus.classList.add("notread");
+    }
+    //readstatus.textContent = book.readstatus ? "Read" : "Not read yet";
   })
+
+  const btncontainer = document.createElement("div");
+  btncontainer.classList.add("btndiv");
+  btncontainer.appendChild(removeBkButton);
+  btncontainer.appendChild(readButton);
 
   card.appendChild(title);
   card.appendChild(author);
   card.appendChild(pages);
   card.appendChild(readstatus);
-  card.appendChild(removeBkButton);
-  card.appendChild(readButton);
+  card.appendChild(btncontainer);
+  /*card.appendChild(removeBkButton);
+  card.appendChild(readButton);*/
 
   bookshelf.appendChild(card);
 }
 
-//addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
-//addBookToLibrary("1984", "George Orwell", 328, true);
-
-//myLibrary.forEach(displayBook);
+addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K Rowling", 309, true);
+addBookToLibrary("Death Note Vol I", "Takeshi Obata", 200, true);
+addBookToLibrary("The Da Vinci Code", "Dan Brown", 480, false);
+addBookToLibrary("The Hunger Games", "Suzanne Collins", 384, true);
+myLibrary.forEach(displayBook);
 
 addnewbk.addEventListener("click", () => {
   dialogbox.showModal();
